19a3781c72153ee1e8107b481b67747dd0e9e9f6
leider <derleider@web.de>
2015-08-08 12:34:39 +0200

    test Methoden besser benannt

--- MCB/src/test/java/mcb/persistenz/TreffenTest.java
+++ MCB/src/test/java/mcb/persistenz/TreffenTest.java
@@ -1,29 +1,30 @@
 package mcb.persistenz;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import mcb.model.Treffen;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class TreffenTest {
 
   @Test
-  public void testIsGespannApril() {
+  public void einNeuesTreffenIstGespann() {
+    assertTrue(new Treffen().isGespann());
+  }
+
+  @Test
+  public void einTreffenImAprilIstGespann() {
     Treffen treffen = new Treffen();
     treffen.setErsterTagString("01.04.2009");
-    Assert.assertTrue(treffen.isGespann());
+    assertTrue(treffen.isGespann());
   }
 
   @Test
-  public void testIsGespannEmpty() {
-    Assert.assertTrue(new Treffen().isGespann());
-  }
-
-  @Test
-  public void testIsGespannOktober() {
+  public void einTreffenImOktoberIstSolo() {
     Treffen treffen = new Treffen();
     treffen.setErsterTagString("01.10.2009");
-    Assert.assertFalse(treffen.isGespann());
+    assertFalse(treffen.isGespann());
   }
 
 }
